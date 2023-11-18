704c6ce96d31e035a920c47c2ff30230a8b37f57
arunisg <89073812+arunisg@users.noreply.github.com>
2023-11-18 13:38:33 +0530

    Fix the https://github.com/mrarunsingh8/videoGPT/commit/ae0634baa5bc556e44061bde2aabc0587ccf3f5b#r132132281

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 require("dotenv").config();
-const app = require("./app");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const routesConfig = require("./configs/routesConfig");
+
+const app = express();
+app.use(express.static('public'));
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+    res.json({ "message": "Hello goes here." });
+});
+
+app.use(routesConfig);
 
 app.listen(3000, ()=>{
     console.log("Server is listen on port 3000");
